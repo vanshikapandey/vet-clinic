99c40024ce3fbc9a8fa705c973a8305de7c76666
vanshikapandey <pvanshika217@gmail.com>
2022-12-18 21:31:28 +0530

    refactored

--- assets/app.js
+++ assets/app.js
@@ -48,3 +48,18 @@ btn.on("click", function (e) {
   e.preventDefault();
   $("html, body").animate({ scrollTop: 0 }, "300");
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
